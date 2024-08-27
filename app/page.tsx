77cbafccfd493f12eba9e9c253ff50860e94b2d3
Ganesh Kunwar <gkunwar09@gmail.com>
2024-08-27 13:35:04 +0545

    Added Voyaaz text

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex   min-h-screen flex-col items-center justify-center bg-[#6CC8BD]">
+      <div className="text-[52px] font-[700] text-[#262671] font-montserrat">
+        Voyaaz
+      </div>
       <div className="text-[52px] font-[700]">
-        Coming Soon
+        Coming Soon...
       </div>
     </main>
   );
